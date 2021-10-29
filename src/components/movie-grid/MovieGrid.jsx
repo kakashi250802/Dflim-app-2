f4b2526fc0f803d3cbaaea56fedabb06a4f2ab2a
kakashi250802 <ngoduong37na@gmail.com>
2021-10-29 22:19:54 +0700

    update

---
+++ src/components/movie-grid/MovieGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './movie-grid.scss';
+const MovieGrid = props => {
+    return (
+        <div>
+            MovieGrid
+        </div>
+    )
+}
+
+export default MovieGrid
